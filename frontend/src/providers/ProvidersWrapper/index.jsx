46dffdb75664a1cb202044653cd38b9734940f85
Mateo González <minidrw@gmail.com>
2023-11-16 17:45:20 -0300

    feat: providers wrapper and include nextui provider

---
+++ frontend/src/providers/ProvidersWrapper/index.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import {NextUIProvider} from '@nextui-org/react'
+const ProvidersWrapper = ({children}) => {
+    return (
+        <NextUIProvider>
+            <BrowserRouter>
+                {children}
+            </BrowserRouter>
+        </NextUIProvider>
+
+    )
+}
+
+export default ProvidersWrapper
