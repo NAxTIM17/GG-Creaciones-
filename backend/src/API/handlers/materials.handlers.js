261251ccad45718e8e9eec992a0f5238a8c85f2b
Joaquin Gonzalez <joaquingonzalez03@gmail.com>
2023-11-24 01:23:07 -0300

    fix: adding column and solving errors

--- backend/src/API/handlers/materials.handlers.js
+++ backend/src/API/handlers/materials.handlers.js
@@ -3,7 +3,7 @@ import db from "../../db";
 export const getMaterials = async(req, resp) => {
     try {
         const result = await db.query(`
-            SELECT material.name AS Nombre, material_stock.value AS Stock, material_cost.value AS Precio 
+            SELECT material.name AS Nombre, material_stock.value AS Stock, material.unit_of_measurement AS Unidad, material_cost.value AS Precio 
             FROM material
             INNER JOIN material_stock ON material.id = material_stock.material_id
             INNER JOIN material_cost ON material.id = material_cost.material_id
@@ -19,11 +19,11 @@ export const getMaterials = async(req, resp) => {
 
 export const addMaterial = async(req, resp) => {
     try {
-        const {nombre, stock, precio} = req.body
-        await db.query('INSERT INTO material (name) VALUES ?', [nombre])
+        const {nombre, stock, unidad, precio} = req.body
+        await db.query('INSERT INTO material (name, unit_of_measurement) VALUES (?, ?)', [nombre, unidad])
         const materialId = await db.query('SELECT LAST_INSERT_ID()').insertId
-        await db.query('INSERT INTO material_stock (material_id, value) VALUES ?', [materialId,stock])
-        await db.query('INSERT INTO material_cost (material_id, value) VALUES ?', [materialId, precio])
+        await db.query('INSERT INTO material_stock (material_id, value) VALUES (?, ?)', [materialId,stock])
+        await db.query('INSERT INTO material_cost (material_id, value) VALUES (?, ?)', [materialId, precio])
         db.release()
         return resp.json({message: "Material agregado exitosamente"})
     } catch (error) {
@@ -36,8 +36,8 @@ export const addMaterial = async(req, resp) => {
 export const updateMaterial = async(req, resp) => {
     try {
         const {id} = req.params
-        const {nombre, stock, precio} = req.body
-        await db.query('UPDATE material SET name = ? WHERE id = ?', [nombre, id])
+        const {nombre, stock, unidad, precio} = req.body
+        await db.query('UPDATE material SET name = ?, unit_of_measurement = ? WHERE id = ?', [nombre, unidad, id])
         await db.query('UPDATE material_stock SET value = ? WHERE material_id = ?', [stock, id])
         await db.query('UPDATE material_cost SET value = ? WHERE material_id = ?', [precio, id])
         db.release()
