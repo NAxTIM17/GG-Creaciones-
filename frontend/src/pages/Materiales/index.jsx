2d76466d8c48730918c82d90e0a110de6ab84047
Walter Mamani <damawal@hotmail.com>
2023-11-25 04:17:28 -0300

    fix:Sales Styles

--- frontend/src/pages/Materiales/index.jsx
+++ frontend/src/pages/Materiales/index.jsx
@@ -1,15 +1,18 @@
 import './Materiales.css'
 import TableTitle from '../../components/Table'
 import productos from './data'
-
+import SearchBar from '../../components/SearchBar'
 
 export default function Materiales(){
     return(
         <>
             <div className="Container">
                 <div className="materiales-container">
+                    <div className="materiales-container-title">
+                        <h1>MATERIALES</h1>
+                    </div>
                     <div className="materiales-container-search">
-
+                        <SearchBar />
                     </div>
                     <div className="materiales-container-table">
                         <TableTitle data={productos}/>
